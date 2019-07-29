04caae9a484a1e7e7141f2b7fe66803bd79042aa
Sudip Adhikari <sudip.adhikari01@gmail.com>
2019-07-29 13:43:04 +0545

    route added successfully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {GstGetComponent} from "./gst-get/gst-get.component";
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
+import {GstAddComponent} from "./gst-add/gst-add.component";
+import {GstUpdateComponent} from "./gst-update/gst-update.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "business/create",
+    component: GstAddComponent
+  }, {
+    path: "business/edit/:id",
+    component: GstUpdateComponent
+  }, {
+    path: "business",
+    component: GstGetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
