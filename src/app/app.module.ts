ab4bce5362212ac8786e64a5228359c3c34fceea
Sudip Adhikari <sudip.adhikari01@gmail.com>
2019-07-29 13:26:18 +0545

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstUpdateComponent } from './gst-update/gst-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GstAddComponent,
+    GstGetComponent,
+    GstUpdateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gst-update/gst-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gst-update',
+  templateUrl: './gst-update.component.html',
+  styleUrls: ['./gst-update.component.css']
+})
+export class GstUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
